999d7009c072b0661ebad81761220d48642aa645
Shriya Dixit <d.shriya@iitg.ac.in>
2021-10-01 22:40:34 +0530

    Change color of debris

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
   baseLayerPicker: false,
   skyBox: new Cesium.SkyBox({
     sources: {
-      positiveX: 'img8.jpeg',
-      negativeX: 'img8.jpeg',
-      positiveY: 'img8.jpeg',
-      negativeY: 'img8.jpeg',
-      positiveZ: 'img8.jpeg',
-      negativeZ: 'img8.jpeg'
+      positiveX: 'img12.jpeg',
+      negativeX: 'img12.jpeg',
+      positiveY: 'img12.jpeg',
+      negativeY: 'img12.jpeg',
+      positiveZ: 'img12.jpeg',
+      negativeZ: 'img12.jpeg'
     }
   }),
 
